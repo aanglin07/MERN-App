6fa8d2e3db65c11e042917f4423b5e693aa44202
aanglin07 <aanglin07@gmail.com>
2023-12-31 04:24:05 -0500

    Added route to get a user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import jwtVerify from '../../middleware/auth.js';
+import Profile from '../../model/Profile.js';
+import User from '../../model/Users.js';
 
 const router = express.Router();
-//@route    GET api/profile
-//@desc     Test route
-//access    Public
-router.get('/', (req, res) => {
+//@route    GET api/profile/me
+//@desc     Get current user's profile
+//access    Private
+router.get('/me', jwtVerify, async (req, res) => {
 
-    res.send('Profile route');
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['firstname', 'lastname', 'avatar']);
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+        } 
+        res.json(profile);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 
 })
 
