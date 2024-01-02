5541f7c5faaa0e5bad00dff9462f1783a2b35458
aanglin07 <aanglin07@gmail.com>
2024-01-02 05:40:54 -0500

    fixed bug - missing parenthesis

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,6 @@ import jwtVerify from '../../middleware/auth.js';
 import Profile from '../../model/Profile.js';
 import { check, validationResult } from 'express-validator';
 import User from '../../model/Users.js';
-import { check, validationResult } from 'express-validator';
 
 
 const router = express.Router();
@@ -102,7 +101,8 @@ router.post('/', [
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-}
+})
+
             
 //@route    GET api/profile/
 //@desc     Get all profile
