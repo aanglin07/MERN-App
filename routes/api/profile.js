54a5b367f4c5cd07eb9f0cecb3719aad2ab14f7b
aanglin07 <aanglin07@gmail.com>
2024-01-02 05:25:56 -0500

    Get All Users and User by user Id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -22,4 +22,122 @@ router.get('/me', jwtVerify, async (req, res) => {
 
 })
 
+//@route    POST api/profile/
+//@desc     Create or update profile
+//access    Private
+
+router.post('/', [
+    jwtVerify,
+    [
+        check('status', 'Status is required').not().isEmpty(),
+        check ('skills', 'Skills is required').not().isEmpty()
+    ]
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+    if(skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    profileFields.social = {};
+    if(youtube) profileFields.social.youtube = youtube;
+    if(twitter) profileFields.social.twitter = twitter;
+    if(facebook) profileFields.social.facebook = facebook;
+    if(linkedin) profileFields.social.linkedin = linkedin;
+    if(instagram) profileFields.social.instagram = instagram;
+
+    try
+    {
+        let profile = await Profile.findOne({ user: req.user.id });
+
+        if(profile)
+        {
+            //Update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            );
+
+            return res.json(profile);
+        }
+
+        //Create
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile);
+    }
+    catch(err)
+    {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route    GET api/profile/
+//@desc     Get all profile
+//access    Public
+
+router.get('/', async(req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['firstname', 'lastname', 'avatar']);
+        res.json(profiles);        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+//@route    GET api/profile/user/:user_id
+//@desc     Get all profile
+//access    Public
+
+router.get('/user/:user_id', async(req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['firstname', 'lastname', 'avatar']);
+        
+        if(!profile) 
+        {
+            return res.status(400).json({msg: 'Profile not found'});
+        }
+        
+        return res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+
+        if (error.kind == 'ObjectId')
+        {
+            return res.status(400).json({msg: 'Profile not found'});
+        }         
+        res.status(500).send('Server Error');
+    }
+})
+
 export default router;
