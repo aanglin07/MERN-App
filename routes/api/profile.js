de5403921e72d4895a446dcd9852e259184d7da5
aanglin07 <aanglin07@gmail.com>
2024-01-03 05:14:14 -0500

    Added a route for User to add experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,7 +144,7 @@ router.get('/user/:user_id', async(req, res) => {
 })
 
 //@route    Delete api/profile/
-//@desc     Delete profile
+//@desc     Delete user and profile
 //access    Private
 
 router.delete('/', jwtVerify, async(req, res) => {
@@ -161,6 +161,55 @@ router.delete('/', jwtVerify, async(req, res) => {
         }
     })
 
+//@route    PUT api/profile/experience
+//@desc     Add profile experience
+//access    Private
+
+router.put('/experience', [jwtVerify, [ 
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async(req, res) =>
+{
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id})
+
+        profile.experience.unshift(newExp); //unshift is the same as push but it pushes to the beginning instead of end
+
+        await profile.save();
+
+        res.json(profile)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+    }
+    )
 
 
 export default router;
