00003d9ee88643c8a7c8201eb7cf1abe0807a5e1
aanglin07 <aanglin07@gmail.com>
2024-01-08 06:44:04 -0500

    Add and delete education routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -233,5 +233,78 @@ router.put('/experience', [jwtVerify, [
         }
     })
 
+//@route    PUT api/profile/education
+//@desc     Add profile education
+//access    Private
+
+router.put('/education', [jwtVerify, [ 
+    check('school', 'School is required').not().isEmpty(),
+    check('degree', 'Degree is required').not().isEmpty(),
+    check('fieldofstudy', 'Field of Study is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async(req, res) =>
+{
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id})
+
+        profile.education.unshift(newEdu); //unshift is the same as push but it pushes to the beginning instead of end
+
+        await profile.save();
+
+        res.json(profile)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+    })
+
+    //@route    DELETE api/profile/education
+    //@desc     Delete profile education
+    //access    Private
+
+    router.delete('/education/:edu_id', [jwtVerify], async(req, res) =>
+    {
+        try {
+            const profile = await Profile.findOne({user: req.user.id}) //gets profile by user id
+
+            //Get remove index(The education we need to remove)
+            const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+            profile.education.splice(removeIndex, 1);
+
+            await profile.save();
+
+            res.json(profile)
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    })
+
 
 export default router;
