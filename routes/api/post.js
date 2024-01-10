43b2d39d5957e9601fde0a0253e1cd3c9f1896ff
aanglin07 <aanglin07@gmail.com>
2024-01-10 10:33:46 -0500

    Added route for a user to create a post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,13 +1,46 @@
 import express from 'express';
+import jwtVerify from '../../middleware/auth.js';
+import { check, validationResult } from 'express-validator';
+import Post from '../../model/Posts.js';
+import Profile from '../../model/Profile.js';
+import User from '../../model/Users.js';
 
 const router = express.Router();
-//@route    GET api/post
-//@desc     Test route
-//access    Public
-router.get('/', (req, res) => {
+//@route    POST api/post
+//@desc     Create a post
+//access    Private
 
-    res.send('Post route');
 
-})
+router.post('/', [
+    check('text', 'Text is required').not().isEmpty(),[jwtVerify]
+    
+], async (req, res) =>
+{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    
+    try {
+        const  user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            avatar: user.avatar,
+            user:req.user.id
+        })
+
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (err) 
+    {  
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})  
 
 export default router;
